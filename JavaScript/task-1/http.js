10925e18c5f9df6193090870dbf9489b7ce36013
Oleksii Novikov <oleksii@novikraft.com>
2024-02-11 18:57:33 +0200

    added scaffolding for the http protocol

--- JavaScript/task-1/http.js
+++ JavaScript/task-1/http.js
@@ -2,6 +2,16 @@
 
 const http = require('node:http');
 
+const COMMON_HEADERS = {
+  'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
+  'Strict-Transport-Security': 'max-age=31536000; includeSubdomains; preload',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Content-Type': 'application/json; charset=UTF-8',
+};
+
 const receiveArgs = async (req) => {
   const buffers = [];
   for await (const chunk of req) buffers.push(chunk);
@@ -11,17 +21,16 @@ const receiveArgs = async (req) => {
 
 module.exports = (routing, { port }) => {
   http.createServer(async (req, res) => {
+    res.writeHead(200, COMMON_HEADERS);
+    if (req.method !== 'POST') return void res.end('Not Found');
     const { url, socket } = req;
-    const [name, method, id] = url.substring(1).split('/');
+    const [place, name, method, id] = url.substring(1).split('/');
+    if (place !== 'api') return void res.end('Not Found');
     const entity = routing[name];
     if (!entity) return void res.end('Not found');
     const handler = entity[method];
     if (!handler) return void res.end('Not found');
-    const src = handler.toString();
-    const signature = src.substring(0, src.indexOf(')'));
-    const args = [];
-    if (signature.includes('(id')) args.push(id);
-    if (signature.includes('{')) args.push(await receiveArgs(req));
+    const { args } = await receiveArgs(req);
     console.log(`${socket.remoteAddress} ${method} ${url}`);
     const result = await handler(...args);
     res.end(JSON.stringify(result.rows));
--- JavaScript/task-1/static/client.js
+++ JavaScript/task-1/static/client.js
@@ -1,44 +1,80 @@
 'use strict';
 
-const socket = new WebSocket('ws://127.0.0.1:8001/');
-
-const scaffold = (structure) => {
+const toApi = (structure, transport) => {
   const api = {};
   const services = Object.keys(structure);
   for (const serviceName of services) {
     api[serviceName] = {};
-    const service = structure[serviceName];
-    const methods = Object.keys(service);
+    const methods = Object.keys(structure[serviceName]);
     for (const methodName of methods) {
-      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-        const packet = { name: serviceName, method: methodName, args };
-        socket.send(JSON.stringify(packet));
-        socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          resolve(data);
-        };
-      });
+      api[serviceName][methodName] = transport(serviceName, methodName);
     }
   }
   return api;
+}
+
+const transports = {};
+
+transports.http = (url, structure) => {
+  const transport = (service, method) => (...args) => new Promise((resolve, reject) => {
+    fetch(`${url}/api/${service}/${method}`, {
+      body: JSON.stringify({ args }),
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+    }).then(res => {
+      if (res.status === 200) resolve(res.json());
+      else reject(new Error(`Status Code: ${res.status}`));
+    });
+  });
+  const api = toApi(structure, transport);
+  return Promise.resolve(api);
+}
+
+transports.ws = (url, structure) => {
+  const socket = new WebSocket(url);
+  const transport = (name, method) => (...args) => new Promise((resolve) => {
+    const packet = { name, method, args };
+    socket.send(JSON.stringify(packet));
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      resolve(data);
+    }
+  });
+  const api = toApi(structure, transport);
+  return new Promise((resolve) => {
+    socket.addEventListener('open', () => resolve(api));
+  });
 };
 
-const api = scaffold({
-  user: {
-    create: ['record'],
-    read: ['id'],
-    update: ['id', 'record'],
-    delete: ['id'],
-    find: ['mask'],
-  },
-  country: {
-    read: ['id'],
-    delete: ['id'],
-    find: ['mask'],
-  },
-});
+const scaffold = (url, structure) => {
+  const protocol = url.slice(0, url.indexOf(':'));
+  return transports[protocol]?.(url, structure) ?? transports.http(url, structure);
+}
 
-socket.addEventListener('open', async () => {
-  const data = await api.user.read(3);
-  console.dir({ data });
-});
+(async () => {
+  const api = await scaffold('http://localhost:8001', {
+    city: {
+      create: ['record'],
+      delete: ['id'],
+      read: ['id'],
+      update: ['id', 'record'],
+    },
+    country: {
+      create: ['record'],
+      delete: ['id'],
+      find: ['mask'],
+      read: ['id'],
+      update: ['id', 'record'],
+    },
+    user: {
+      create: ['record'],
+      delete: ['id'],
+      find: ['mask'],
+      read: ['id'],
+      update: ['id', 'record'],
+    },
+  });
+  // await api.user.create({ login: 'm3', password: 'test' });
+  const data = await api.user.read();
+  console.log(data);
+})();
